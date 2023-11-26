3db81320d59e7797055c851848890051bdfb7cc3
Gerardo Galindez <ggalindezb@outlook.com>
2023-11-26 12:34:00 -0600

    Add basic schema

--- db.sql
+++ db.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS "posts" (
   created_on INTEGER NOT NULL,
   updated_on INTEGER NOT NULL
 );
+
+INSERT INTO POSTS(slug, content, created_on, updated_on)
+VALUES('welcome', 'A welcome post', strftime('%Y-%m-%d %H:%M:%S', datetime('now')), strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
+
+INSERT INTO POSTS(slug, content, created_on, updated_on)
+VALUES('something', 'Something else blog post', strftime('%Y-%m-%d %H:%M:%S', datetime('now')), strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
