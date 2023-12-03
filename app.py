3c979aaa35ff589997db9bc67774f07ed807d53b
Gerardo Galindez <ggalindezb@outlook.com>
2023-12-03 17:12:10 -0600

    Replace string quoting

--- app.py
+++ app.py
@@ -4,23 +4,23 @@ import auth
 
 app = Flask(__name__)
 
-@app.route("/login", methods=["POST"])
+@app.route('/login', methods=['POST'])
 def login():
     secret = int(request.get_json()['secret'])
     token = auth.generate_token(secret)
     return {"jwt": token}
 
-@app.route("/posts")
+@app.route('/posts')
 def fetch_posts():
     posts = PostModel()
     return posts.all()
 
-@app.route("/posts/<slug>")
+@app.route('/posts/<slug>')
 def fetch_post(slug):
     posts = PostModel()
     return posts.find(slug)
 
-@app.route("/posts", methods=["POST"])
+@app.route('/posts', methods=['POST'])
 @auth.validate_token
 def create_post():
     slug = request.get_json()['slug']
@@ -29,7 +29,7 @@ def create_post():
     posts.create(slug, content)
     return ('', 201)
 
-@app.route("/posts/<slug>", methods=["PUT"])
+@app.route('/posts/<slug>', methods=['PUT'])
 @auth.validate_token
 def update_post(slug):
     content = request.get_json()['content']
@@ -37,12 +37,12 @@ def update_post(slug):
     posts.update(slug, content)
     return ('', 200)
 
-@app.route("/posts/<slug>", methods=["DELETE"])
+@app.route('/posts/<slug>', methods=['DELETE'])
 @auth.validate_token
 def delete_post(slug):
     posts = PostModel()
     posts.delete(slug)
     return ('', 200)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
