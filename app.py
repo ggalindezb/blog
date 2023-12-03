8dc0b285902362aef22cac339f3f602ebd7972d5
Gerardo Galindez <ggalindezb@outlook.com>
2023-12-03 17:07:09 -0600

    Enforce validation on write endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, g
 from post_model import PostModel
 import auth
 
@@ -11,12 +11,17 @@ def login():
     return {"jwt": token}
 
 @app.route("/posts")
-@auth.validate_request
 def fetch_posts():
     posts = PostModel()
     return posts.all()
 
+@app.route("/posts/<slug>")
+def fetch_post(slug):
+    posts = PostModel()
+    return posts.find(slug)
+
 @app.route("/posts", methods=["POST"])
+@auth.validate_token
 def create_post():
     slug = request.get_json()['slug']
     content = request.get_json()['content']
@@ -24,12 +29,8 @@ def create_post():
     posts.create(slug, content)
     return ('', 201)
 
-@app.route("/posts/<slug>")
-def fetch_post(slug):
-    posts = PostModel()
-    return posts.find(slug)
-
 @app.route("/posts/<slug>", methods=["PUT"])
+@auth.validate_token
 def update_post(slug):
     content = request.get_json()['content']
     posts = PostModel()
@@ -37,6 +38,7 @@ def update_post(slug):
     return ('', 200)
 
 @app.route("/posts/<slug>", methods=["DELETE"])
+@auth.validate_token
 def delete_post(slug):
     posts = PostModel()
     posts.delete(slug)
--- auth.py
+++ auth.py
@@ -2,31 +2,21 @@ import jwt
 import os
 import functools
 import flask
-import pdb
+from flask import request, g
 
 JWT_TOKEN = os.environ['JWT_TOKEN']
 
 def generate_token(secret):
     return jwt.encode({'user_id': secret}, JWT_TOKEN, algorithm='HS256')
 
-def validate_token(token):
-    return jwt.decode(token, JWT_TOKEN, algorithm='HS256')
-
-def wrapper(request):
-    print('Outside')
-    print(request)
-    def inner(func):
-        print('Wrapper')
-        return func
-    return inner
-
-def validate_request(f):
+def validate_token(f):
     @functools.wraps(f)
-    def decorated_function(*args, **kwargs):
+    def token_validation(*args, **kwargs):
         try:
-            bearer_token = flask.request.headers['Authorization'].split().pop()
-            jwt.decode(bearer_token, JWT_TOKEN, algorithms=['HS256'])
+            bearer_token = request.headers['Authorization'].split().pop()
+            token = jwt.decode(bearer_token, JWT_TOKEN, algorithms=['HS256'])
+            g.token = token
         except KeyError:
             flask.abort(401)
         return f(*args, **kwargs)
-    return decorated_function
+    return token_validation
