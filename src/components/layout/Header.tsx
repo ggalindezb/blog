fd95508d25653d7b537a874579b2dc87f5ea0061
Gerardo Galindez <ggalindezb@outlook.com>
2024-02-21 12:06:44 -0600

    Update markup

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,20 @@
+import './Header.css'
+
 export default function Header() {
   return (
     <header>
-      <img src="/logo.png" alt="log" />
-      <nav>
-        <ul>
-          <li>Posts</li>
-          <li>TIL</li>
-          <li>Snips</li>
-        </ul>
-      </nav>
+      <div className="logo">
+        <img src="/logo.png" alt="log" />
+      </div>
+      <div className="navigation">
+        <nav>
+          <ul>
+            <li>Posts</li>
+            <li>TIL</li>
+            <li>Snips</li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
