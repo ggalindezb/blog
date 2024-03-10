96a5fe03d5783d6a6dfb586ca2bc6273ca48607d
Gerardo Galindez <ggalindezb@outlook.com>
2024-03-09 21:04:33 -0600

    Add changelog, add auth blueprint, clean up model db fetch

---
+++ blog/auth.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify
+from blog.models.user_model import UserModel
+from blog.services.auth import generate_token
+
+blueprint = Blueprint('auth', __name__, url_prefix='/auth')
+
+@blueprint.route('/login', methods=['POST'])
+def login():
+    key = request.get_json()['key']
+    user = UserModel.find_by(key)
+    token = generate_token(user)
+    return jsonify({"jwt": token})
