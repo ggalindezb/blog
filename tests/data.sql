b5b436dd24dc835d3e3c1855925539552c6cb787
Gerardo Galindez <ggalindezb@outlook.com>
2024-03-18 13:01:20 -0600

    Add test data for fixtures

--- tests/data.sql
+++ tests/data.sql
@@ -6,26 +6,22 @@ VALUES('00000000-0000-0000-0000-000000000000',
 
 -- Posts
 INSERT INTO posts(slug, brief, content, created_on, updated_on)
-VALUES('post-1',
-  readfile('Post 1 slug'),
-  readfile('Post 1 content'),
+VALUES('post-1-slug',
+  'Post 1 content',
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')),
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
 INSERT INTO posts(slug, brief, content, created_on, updated_on)
-VALUES('post-2',
-  readfile('Post 2 slug'),
-  readfile('Post 2 content'),
+VALUES('post-2-slug',
+  'Post 2 content',
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')),
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
 INSERT INTO posts(slug, brief, content, created_on, updated_on)
-VALUES('post-3',
-  readfile('Post 3 slug'),
-  readfile('Post 3 content'),
+VALUES('post-3-slug',
+  'Post 3 content',
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')),
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
 INSERT INTO posts(slug, brief, content, created_on, updated_on)
-VALUES('post-4',
-  readfile('Post 4 slug'),
-  readfile('Post 4 content'),
+VALUES('post-4-slug',
+  'Post 4 content',
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')),
   strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
