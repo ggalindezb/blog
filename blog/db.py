9fa5bf05a182df7514f099eafab7542325e1c2f1
Gerardo Galindez <ggalindezb@outlook.com>
2024-11-30 07:47:11 -0600

    Use db config

--- blog/db.py
+++ blog/db.py
@@ -1,11 +1,10 @@
 from sqlite4 import SQLite4
 
-import click
 from flask import current_app, g
 
 def get_db():
     if 'db' not in g:
-        g.db = SQLite4('blog/db/blog-db-dev')
+        g.db = SQLite4(current_app.config['DATABASE'])
         g.db.connect()
 
     return g.db
