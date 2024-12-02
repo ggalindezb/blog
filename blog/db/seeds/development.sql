056eb902061fe0508396d97da60eb4d0e7ec65b5
Gerardo Galindez <ggalindezb@outlook.com>
2024-12-01 18:59:24 -0600

    Add temp test ref

--- blog/db/seeds/development.sql
+++ blog/db/seeds/development.sql
@@ -11,3 +11,5 @@ INSERT INTO posts(slug, content, created_on, updated_on)
 VALUES('leaf-compost', readfile('blog/db/seeds/posts/c.html'), strftime('%Y-%m-%d %H:%M:%S', datetime('now')), strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
 INSERT INTO posts(slug, content, created_on, updated_on)
 VALUES('minima-blog-with-flask-and-html', readfile('blog/db/seeds/posts/d.html'), strftime('%Y-%m-%d %H:%M:%S', datetime('now')), strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
+INSERT INTO posts(slug, content, created_on, updated_on)
+VALUES('test', readfile('blog/db/seeds/posts/test.html'), strftime('%Y-%m-%d %H:%M:%S', datetime('now')), strftime('%Y-%m-%d %H:%M:%S', datetime('now')));
