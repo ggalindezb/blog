16f227c5a8a01de329da2226251d726e00d2b5cd
Gerardo Galindez <ggalindezb@outlook.com>
2025-01-01 18:23:26 -0600

    Remove global connection (for now)

--- blog/db.py
+++ blog/db.py
@@ -2,10 +2,6 @@ from flask import current_app, g
 from pymongo import MongoClient
 
 def get_mongo_db():
-    # if 'mongo_db' not in g:
-    #     g.mongo_db = getattr(MongoClient(), current_app.config['DATABASE'])
-
-    # return g.mongo_db
     return MongoClient().blog
 
 def close_db(__exception__):
